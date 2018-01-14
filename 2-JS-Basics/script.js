8cbf0989111bf11fb6aac4988874b91fd2ebdeae
Joe <jnagel88@gmail.com>
2018-01-13 21:47:06 -0600

    completed sec2 lec9

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,14 @@
 console.log('hello World');
+
+var name = 'John';
+console.log(name);
+
+var lastName = 'smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true
+console.log(fullAge)
+
