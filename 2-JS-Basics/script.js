2520d05d002d9ab642a21ccf3219297e5f61ede7
Joe <jnagel88@gmail.com>
2018-01-15 15:08:01 -0600

    completed sec2 lec11

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,14 +1,39 @@
-console.log('hello World');
+//Variable lecture 1
+// console.log('hello World');
 
+// var name = 'John';
+// console.log(name);
+
+// var lastName = 'smith';
+// console.log(lastName);
+
+// var age = 26;
+// console.log(age);
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// lecture variables 2
 var name = 'John';
-console.log(name);
-
-var lastName = 'smith';
-console.log(lastName);
-
 var age = 26;
-console.log(age);
 
-var fullAge = true
-console.log(fullAge)
+// console.log(name + age);
+
+
+// var job, isMarried;
+
+// console.log(job)
+
+job = 'teacher'
+isMarried = false
+
+console.log(name + ' is a ' + age + " year old " + job +'. Is he married? ' + isMarried +'.');
+
+age = "thirty six";
+job = "driver";
+
+var lastName = promt("what is your last name?")
+console.log(lastName)
+
+alert(name + ' is a ' + age + " year old " + job +'. Is he married? ' + isMarried +'.');
 
