34d531e4ce72bc51d95e8dd3274512527e141cdd
Joe <jnagel88@gmail.com>
2018-01-15 15:23:10 -0600

    completed sec2 lec12

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,39 +1,71 @@
-//Variable lecture 1
-// console.log('hello World');
+// //Variable lecture 1
+// // console.log('hello World');
 
+// // var name = 'John';
+// // console.log(name);
+
+// // var lastName = 'smith';
+// // console.log(lastName);
+
+// // var age = 26;
+// // console.log(age);
+
+// // var fullAge = true;
+// // console.log(fullAge);
+
+// // lecture variables 2
 // var name = 'John';
-// console.log(name);
-
-// var lastName = 'smith';
-// console.log(lastName);
-
 // var age = 26;
-// console.log(age);
 
-// var fullAge = true;
-// console.log(fullAge);
-
-// lecture variables 2
-var name = 'John';
-var age = 26;
-
-// console.log(name + age);
+// // console.log(name + age);
 
 
-// var job, isMarried;
+// // var job, isMarried;
 
-// console.log(job)
+// // console.log(job)
 
-job = 'teacher'
-isMarried = false
+// job = 'teacher'
+// isMarried = false
 
-console.log(name + ' is a ' + age + " year old " + job +'. Is he married? ' + isMarried +'.');
+// console.log(name + ' is a ' + age + " year old " + job +'. Is he married? ' + isMarried +'.');
 
-age = "thirty six";
-job = "driver";
+// age = "thirty six";
+// job = "driver";
 
-var lastName = promt("what is your last name?")
-console.log(lastName)
+// var lastName = promt("what is your last name?")
+// console.log(lastName)
 
-alert(name + ' is a ' + age + " year old " + job +'. Is he married? ' + isMarried +'.');
+// alert(name + ' is a ' + age + " year old " + job +'. Is he married? ' + isMarried +'.');
 
+
+
+
+
+
+
+
+
+
+
+
+// Lecture 12: Operators
+
+var now = 2018
+var birthYear = now - 26;
+
+
+
+birthYear = now - 26 *2;
+console.log(birthYear);
+
+var agejohn = 30;
+var agemark = 30;
+
+agejohn = agemark =(3 + 5) * 4 - 6;
+console.log(agejohn);
+console.log(agemark);
+
+agemark *= 2;
+agejohn++
+console.log(agejohn);
+console.log(agemark);
