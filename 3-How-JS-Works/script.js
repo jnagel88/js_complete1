d5c8cdf327ec08c485070b1ac85069901f36ac9e
Joe <jnagel88@gmail.com>
2018-01-20 20:40:04 -0600

    completed Section 3 Lecture 31.

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -1,36 +1,36 @@
 ///////////////////////////////////////
 // Lecture 30: Hoisting
 
-// functions
-calcage(1988);
+// // functions
+// calcage(1988);
 
-function calcage(year) {
-    console.log(2018 - year);
-}
-
-
-
-// retirement(1988);
-var retirement  = function(year) {
-    console.log(65 - (2018-year));
-}
+// function calcage(year) {
+//     console.log(2018 - year);
+// }
 
 
 
-// variation
+// // retirement(1988);
+// var retirement  = function(year) {
+//     console.log(65 - (2018-year));
+// }
 
-console.log(age);
-var age = 23;
-console.log(age);
 
-function foo() {
-    console.log(age);
-    var age = 65;
-    console.log(age);
-}
 
-foo();
-console.log(age);
+// // variation
+
+// console.log(age);
+// var age = 23;
+// console.log(age);
+
+// function foo() {
+//     console.log(age);
+//     var age = 65;
+//     console.log(age);
+// }
+
+// foo();
+// console.log(age);
 
 
 
@@ -41,44 +41,44 @@ console.log(age);
 
 // First scoping example
 
-/*
-var a = 'Hello!';
-first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// var a = 'Hello!';
+// first();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
+// function first() {
+//     var b = 'Hi!';
+//     second();
+
+//     function second() {
+//         var c = 'Hey!';
+//         console.log(a + b + c);
+//     }
+// }
 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
-var a = 'Hello!';
-first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// var a = 'Hello!';
+// first();
 
-    function second() {
-        var c = 'Hey!';
-        third()
-    }
-}
+// function first() {
+//     var b = 'Hi!';
+//     second();
 
-function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
-}
-*/
+//     function second() {
+//         var c = 'Hey!';
+//         third()
+//     }
+// }
+
+// function third() {
+//     var d = 'John';
+//     // console.log(a + b + c + d);
+//     console.log(a+d)
+// }
+
 
 
 
