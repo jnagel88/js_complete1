55a900e4f770e13821eb53fc591a6ae888439b4c
Joe <jnagel88@gmail.com>
2018-01-20 19:59:06 -0600

    initial add of Section 3 files.

---
+++ 3-How-JS-Works/script.js
@@ -0,0 +1,77 @@
+///////////////////////////////////////
+// Lecture 30: Hoisting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////
+// Lecture 31: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture 32: The this keyword
+
+
+
+
+
+
+
+
+
