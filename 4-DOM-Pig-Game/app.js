0b18945242ea972cdc3d15046b0ea9f3f49944d6
Joe Nagel <jnagel88@gmail.com>
2018-01-22 16:24:15 -0600

    Completed lecture 41. Added round score functionality.

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -13,7 +13,7 @@ var scores, roundscore, activeplayer;
 
 scores = [0, 0];
 roundscore = 0;
-activePlayer = 1; 
+activeplayer = 0; 
 
 
 document.querySelector('.dice').style.display = 'none';
@@ -26,14 +26,34 @@ document.getElementById('current-1').textContent = '0';
 // Dice roller
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	// 1. random number
-	var dice = Math.floor(Math.random()*6+1);
+	var dice = Math.floor(Math.random() * 6 + 1);
 
 	// 2. display the result
 	var diceDOM = document.querySelector('.dice')
 	diceDOM.style.display = 'block';
 	diceDOM.src = 'dice-' + dice + '.png';
 
-	//3. updatre the round score IF the roll is not a 1
+	//3. update the round score IF the roll is not a 1
+	if (dice !== 1){
+		// add score
+		roundscore += dice;
+		document.querySelector('#current-'+ activeplayer).textContent = roundscore;
+	} else{
+		// next player, round score 0
+		activeplayer === 0 ? activeplayer = 1 :activeplayer = 0;
+		roundscore = 0;
+
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		// document.querySelector('.player-0-panel').classList.remove('active');
+		// document.querySelector('.player-1-panel').classList.add('active');
+
+		document.querySelector('.dice').style.display = 'none';
+	}
 
 
 
