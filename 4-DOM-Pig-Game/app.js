42153cc03bb91363e39408adfb9c0190de9ea138
Joe Nagel <jnagel88@gmail.com>
2018-01-22 14:53:52 -0600

    completed section 4 lectures 36-40.

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -6,5 +6,50 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
+
+// initial Variable definitions
+var scores, roundscore, activeplayer;
+
+scores = [0, 0];
+roundscore = 0;
+activePlayer = 1; 
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+// Dice roller
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	// 1. random number
+	var dice = Math.floor(Math.random()*6+1);
+
+	// 2. display the result
+	var diceDOM = document.querySelector('.dice')
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+	//3. updatre the round score IF the roll is not a 1
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('#current-'+ activePlayer).textContent = dice;
+// alt method to utilize HTML elements in the output.
+// document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice + '</em>';
+// var x= document.querySelector('#score-0').textContent;
